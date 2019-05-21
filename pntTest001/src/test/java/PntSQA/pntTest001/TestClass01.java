c3278b32a9ea4b7875f5e6813a0a9ee9b470b56c
MH-PC <MH-PC@192.168.1.14>
2019-05-21 23:57:29 +0600

    First Commit

---
+++ pntTest001/src/test/java/PntSQA/pntTest001/TestClass01.java
@@ -0,0 +1,13 @@
+package PntSQA.pntTest001;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TestClass01 {
+	
+	@Test
+	public void Test01() {
+	
+	}
+}
